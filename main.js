d77ca1f017e4fe0d7bfff4bd3177e58f02fab552
shi-chika <hygge34living@gmail.com>
2025-07-28 01:21:43 +0900

    新增RWD

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 import './assets/scss/all.scss';
 
 console.log('Hello world');
+
+document.addEventListener('DOMContentLoaded', () => {
+  const buttons = document.querySelectorAll('.--number');
+
+  buttons.forEach(button => {
+    button.addEventListener('click', () => {
+      // 移除其他按鈕的 active class
+      buttons.forEach(btn => btn.classList.remove('active'));
+
+      // 為目前點擊的按鈕加上 active class
+      button.classList.add('active');
+    });
+  });
+});
